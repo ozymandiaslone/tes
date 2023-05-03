015bb34ee6b6e178436043b86f488766dea1b389
ozymandiaslone <saltsperkins@gmail.com>
2023-05-02 21:07:39 -0600

    setup.sh

---
+++ llmserver/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp go-llama.cpp/binding.cpp binding.cpp
+cp go-llama.cpp/binding.h binding.h
+cp go-llama.cpp/binding.o binding.o
+cp go-llama.cpp/go.mod go.mod
+cp go-llama.cpp/go.sum go.sum
+cp go-llama.cpp/lama_test.go lama_test.go
+cp go-llama.cpp/libbinding.a libbinding.a
+cp -r go-llama.cpp/llama.cpp llama.cpp
+cp go-llama.cpp/llama.go llama.go
+cp go-llama.cpp/llama_suite_test.go
+cp go-llama.cpp/options.go options.go
